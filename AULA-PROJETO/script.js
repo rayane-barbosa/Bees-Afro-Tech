33ef4a06125960f71dfdb60ac2276e77ff909519
Rayane Barbosa <rayane.c.barbosa@gmail.com>
2023-04-23 13:44:42 -0300

    correções projeto

--- AULA-PROJETO/script.js
+++ AULA-PROJETO/script.js
@@ -1,22 +1,20 @@
 /*Validação do campo e-mail e campo telefone recebidos via input no html*/
 function validarForm() {
-  /* Obter os valores dos campos */
   var email = document.getElementById("email").value;
   var telefone = document.getElementById("telefone").value;
-
-
+  
   var emailRegExp = /^\S+@\S+\.\S+$/;
   var telefoneRegExp = /\d{3}[\s.-]?\d{4}[\s.-]?\d{4}/;
-
-
+  
   if (!emailRegExp.test(email)) {
     alert("Por favor, insira um endereço de e-mail válido.");
     return false;
   }
-
+  
   if (!telefoneRegExp.test(telefone)) {
     alert("Por favor, insira um número de telefone válido.");
     return false;
   }
+  
   return true;
 }
