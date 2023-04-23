b09dd9cd32cb1ba3dbefcb5dba1e51e342b55704
Rayane Barbosa <rayane.c.barbosa@gmail.com>
2023-04-22 23:37:19 -0300

    formulario

--- AULA-PROJETO/script.js
+++ AULA-PROJETO/script.js
@@ -1,7 +1,20 @@
-function validateForm() {
-    let x = document.forms["myForm"]["fname"].value;
-    if (x == "@") {
-      alert("Name must be filled out");
+function validarForm() {
+    var email = document.getElementById("email").value;
+    var telefone = document.getElementById("telefone").value;
+    
+    var emailRegExp = /^\S+@\S+\.\S+$/;
+    var telefoneRegExp = /\d{3}[\s.-]?\d{4}[\s.-]?\d{4}/;
+    
+    if (!emailRegExp.test(email)) {
+      alert("Por favor, insira um endereço de e-mail válido.");
       return false;
     }
+    
+    if (!telefoneRegExp.test(telefone)) {
+      alert("Por favor, insira um número de telefone válido.");
+      return false;
+    }
+    
+    return true;
   }
+  
