36e9f14f4b5dfc655043e3c1830dcd5bef96f436
rayane-barbosa <rayane.c.barbosa@gmail.com>
2023-04-27 15:20:22 -0300

    Menu-react

--- aula-07/src/App.js
+++ aula-07/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Menu from './contents/Menu/Menu';
+import './contents/Menu/Menu.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu 
+        title="O restaurante no fim do universo"
+        menuItems={["Cardápio", "Sobre", "Contato"]}
+        />
+        <main>
+          {/* Conteudo */}
+        </main>
+      
     </div>
   );
 }
---
+++ aula-07/src/contents/Menu/Menu.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Menu.css";
+
+
+
+function Menu(props) {
+    return (
+        <header className="Menu">
+            <h1>{props.title}</h1>
+            <nav>
+                <ul>
+                    {props.menuItems.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </nav>
+
+        </header>
+    );
+}
+
+export default Menu;
