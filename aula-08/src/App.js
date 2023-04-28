a8d0d36c05702446efb75f532633b50c00c05bf5
rayane-barbosa <rayane.c.barbosa@gmail.com>
2023-04-27 22:46:23 -0300

    header-h1

--- aula-08/src/App.js
+++ aula-08/src/App.js
@@ -1,10 +1,25 @@
 import icone from './icone.png'
 import './App.css';
+import Header from './Componentes/Header/Header.jsx';
 
 function App() {
   return (
     <div className="App">
+
+      <Header
+      titulo="Meu feed"/>
       
+      
+      {/* HEADER - MEU FEED
+        IMAGEM - ICONE*/}
+
+      {/* INPUT*/}
+
+      {/* BUTTON*/}
+
+      {/* 3 POST
+        BOTÕES*/}
+   
     </div>
   );
 }
