ad89597d01504f21b414cbb8ca6b5cb0bb49803a
rayane-barbosa <rayane.c.barbosa@gmail.com>
2023-04-30 14:32:51 -0300

    import-componentes

--- projeto-minha-estante-landing-page/src/App.js
+++ projeto-minha-estante-landing-page/src/App.js
@@ -1,23 +1,15 @@
-/*import logo from './logo.svg'; */
+import Header from './components/header';
+import Livro from './components/livro';
+import CardAvaliacao from './components/card-avalicao';
+import Catalogo from './components/catalogo';
+import Menu from './components/Menu';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-  </header> */}
+     {/** Criação dos componentes react */}
     </div>
   );
 }
