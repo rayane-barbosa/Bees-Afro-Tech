4ebbbddfe098f29810b7641b3820cdc18d914fde
rayane-barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2023-05-06 22:19:40 +0000

    Catalogo-componente-react

---
+++ minha-estante-projeto-rotas/src/Componentes/Catalogo/Catalogo.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import LivroAvaliado from '../LivroAvaliado/LivroAvaliado.jsx';
+import './Catalogo.css';
+
+function Catalogo(props){
+    return(
+        <div className='catalogo'>
+            {props.livros.map(book => (
+                <LivroAvaliado
+                key={book.id}
+                title={book.title}
+                description = {book.description}
+                image={book.image}
+                rating={book.rating}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default Catalogo;
